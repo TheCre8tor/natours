de363af3166307fd6f80be8d8fd12f3d55579ddb
Alexander Nitiola <cre8tor.alexander@gmail.com>
2021-03-25 15:08:27 +0100

    Adding Document to the database

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
-const { checkBody, getAllTours, createTour, getTour, updateTour, deleteTour } = tourController;
+const { getAllTours, createTour, getTour, updateTour, deleteTour } = tourController;
 
-router.route('/').get(getAllTours).post(checkBody, createTour);
+router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
