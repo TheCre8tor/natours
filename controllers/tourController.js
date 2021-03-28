c4c378bc488d1ebdc95e0293d3bfa31fe1d993bb
Alexander Nitiola <cre8tor.alexander@gmail.com>
2021-03-28 08:57:54 +0100

    Removed top comment

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,13 +1,5 @@
 const Tour = require('./../models/tourModel');
 
-// <!-- Aliasing Middleware Function -->
-exports.aliasTopTours = (req, res, next) => {
-    req.query.limit = '5';
-    req.query.sort = '-ratingsAverage,-price';
-    req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
-    next();
-};
-
 exports.getAllTours = async (req, res) => {
     try {
         // 1. BUILD QUERY  -->  Implementing Pagination and Sorting
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,9 +2,8 @@ const express = require('express');
 const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
-const { aliasTopTours, getAllTours, createTour, getTour, updateTour, deleteTour } = tourController;
+const { getAllTours, createTour, getTour, updateTour, deleteTour } = tourController;
 
-router.route('/top-five-ratings').get(aliasTopTours, getAllTours);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
