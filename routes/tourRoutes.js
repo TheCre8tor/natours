bb53a476511af349d29f363e3df6b9918fb7fafb
Alexander Nitiola <cre8tor.alexander@gmail.com>
2021-03-29 06:36:47 +0100

    Learn't methods for getting response statistics

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
-const { aliasTopTours, getAllTours, createTour, getTour, updateTour, deleteTour } = tourController;
+const { aliasTopTours, getAllTours, createTour, getTour, updateTour, deleteTour, getTourStats } = tourController;
 
 router.route('/top-five-ratings').get(aliasTopTours, getAllTours);
+router.route('/tour-stats').get(getTourStats);
+
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
