db1347252d66ed383fb4fc7604383bfb6a71cb85
Alexander Nitiola <cre8tor.alexander@gmail.com>
2021-03-29 20:50:39 +0100

    Implement Mongoose Document Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 // DATABASE SCHEMA  -->
 const tourSchema = new mongoose.Schema(
@@ -9,6 +10,7 @@ const tourSchema = new mongoose.Schema(
             unique: true,
             trim: true
         },
+        slug: String,
         duration: {
             type: Number,
             required: [true, 'A tour must have a duration']
@@ -67,6 +69,22 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
     return this.duration / 7;
 });
+
+// DOCUMENT MIDDLEWARE: Runs before .save() and .create() and not .insertMany()
+tourSchema.pre('save', function (next) {
+    this['slug'] = slugify(this.name, { lower: true });
+    next();
+});
+
+// tourSchema.pre('save', function (next) {
+//     console.log('We can have multiple pre() middleware');
+//     next();
+// });
+//
+// tourSchema.post('save', function (doc, next) {
+//     console.log(doc);
+//     next();
+// });
 
 // DATABASE MODEL -->
 const Tour = mongoose.model('Tour', tourSchema);
