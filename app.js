d472b49f88fe7746bff9c7a89f88c4712c2698b1
Alexander Nitiola <cre8tor.alexander@gmail.com>
2021-04-03 18:26:36 +0100

    Error Handling Basics

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appErrors');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -28,29 +30,14 @@ app.use('/api/v1/users', userRouter);
   If we are able to reach this point, then it means
   the req and res circle as not yet finished. */
 app.all('*', (req, res, next) => {
-    // res.status(404).json({
-    //     status: 'failed',
-    //     message: `Can't find ${req.originalUrl} on this server`
-    // });
+    // const error = new Error(`Can't find ${req.originalUrl} on this server`);
+    // error.status = 'General Error: failed';
+    // error.statusCode = 404;
 
-    const error = new Error(`Can't find ${req.originalUrl} on this server`);
-    error.status = 'General Error: failed';
-    error.statusCode = 404;
-
-    next(error);
+    next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
 // GLOBAL ERROR HANDLING MIDDLEWARE
-app.use((error, req, res, next) => {
-    error.statusCode = error.statusCode || 500;
-    error.status = error.status || 'errors';
-
-    res.status(error.statusCode).json({
-        status: error.status,
-        message: error.message
-    });
-
-    next();
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ controllers/errorController/index.js
@@ -0,0 +1,11 @@
+module.exports = (error, req, res, next) => {
+    error.statusCode = error.statusCode || 500;
+    error.status = error.status || 'errors';
+
+    res.status(error.statusCode).json({
+        status: error.status,
+        message: error.message
+    });
+
+    next();
+};
