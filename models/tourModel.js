7215ac4660b57ea33b374a9899958e887d6078ea
Alexander Nitiola <cre8tor.alexander@gmail.com>
2021-04-03 04:55:27 +0100

    Data Built-In Validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,7 +8,9 @@ const tourSchema = new mongoose.Schema(
             type: String,
             required: [true, 'A tour must have a price'],
             unique: true,
-            trim: true
+            trim: true,
+            maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+            minlength: [10, 'A tour name must have more or equal than 10 characters']
         },
         slug: String,
         duration: {
@@ -22,11 +24,17 @@ const tourSchema = new mongoose.Schema(
         difficulty: {
             type: String,
             required: [true, 'A tour must have a difficulty'],
-            trim: true
+            trim: true,
+            enum: {
+                values: ['easy', 'medium', 'difficult'],
+                message: 'Difficulty should be either: easy, medium or difficult'
+            }
         },
         ratingsAverage: {
             type: Number,
-            default: 4.5
+            default: 4.5,
+            min: [1, 'Rating must be above 1.0'],
+            max: [5, 'Rating must be equal to or below 5.0']
         },
         ratingsQuantity: {
             type: Number,
@@ -111,6 +119,7 @@ tourSchema.pre('aggregate', function (next) {
 
     next();
 });
+
 // DATABASE MODEL -->
 const Tour = mongoose.model('Tour', tourSchema);
 
