09081b30cb9d8eeacfb4fb6ed402c686831e06f1
Alexander Nitiola <cre8tor.alexander@gmail.com>
2021-04-05 17:04:48 +0100

    Completed Error Handling with Express Course

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+// <!-- Global Uncaught Exception | Error Handling -->
+process.on('uncaughtException', error => {
+    console.log(`${error.name}: ${error.message.replace(' :', ',')}`);
+    console.log('Uncaught Exception!, Shutting down...');
+    process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 console.log(`We are currently on: ${process.env.NODE_ENV} environment`);
 
@@ -21,6 +28,15 @@ mongoose
 
 // SERVER RUNNER -->
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on ${port}...`);
+});
+
+// GLOBAL UNHANDLED REJECTION - ERROR HANDLING -->
+process.on('unhandledRejection', error => {
+    console.log(`${error.name}: ${error.message.replace(' :', ',')}`);
+    console.log('Unhandeled Rejection!, Shutting down...');
+    server.close(() => {
+        process.exit(1);
+    });
 });
