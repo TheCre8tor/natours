30066aac32e7288070e6a3466d3b7f38a5470ba9
Alexander Nitiola <cre8tor.alexander@gmail.com>
2021-04-23 16:39:33 +0100

    Improved stripe implementation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -48,12 +48,30 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 //     await Booking.create({ tour, user, price });
 
 //     res.redirect(req.originalUrl.split('?')[0]);
-// }); 
+// });
 
-exports.webhookCheckout = (req, res, next) => {
+const createBookingCheckout = async session => {
+    const tour = session.client_reference_id;
+    const user = (await User.findOne({ email: session.customer_email })).id;
+    const price = session.line_items[0].amount / 100;
+    await Booking.create({ tour, user, price });
+};
+
+exports.webhookCheckout = async (req, res, next) => {
     const signature = req.header['stripe-signature'];
-    const event = stripe.webhooks.constructEvent(req.body, signature, )
-}
+
+    let event;
+    try {
+        event = stripe.webhooks.constructEvent(req.body, signature, process.env.STRIPE_WEBHOOK_SECRET);
+    } catch (err) {
+        return res.status(400).send(`Webhook error: ${err.message}`);
+    }
+
+    if (event.type === 'checkout.session.completed') {
+        await createBookingCheckout(event.data.object);
+    }
+    res.status(200).json({ received: true });
+};
 
 exports.createBooking = factory.createOne(Booking);
 exports.getBooking = factory.getOne(Booking);
